b958ca0f16da0babf4508665879995cfbc8c3c81
gulyasm <mgulyas86@gmail.com>
2013-10-14 16:09:50 +0200

    Changed to accept argument from commandline

--- gitutil.py
+++ gitutil.py
@@ -9,18 +9,16 @@ import git
 def main(argv):
     folder = ''
     try:
-        opts, args = getopt.getopt(argv, 'f', ["folder="])
+        opts, args = getopt.getopt(argv, 'r', ["root="])
     except getopt.GetoptError:
-        print('gitutil.py command')
+        print('Usage: gitutil.py -r')
         sys.exit(2)
 
     for opt, arg in opts:
-        if opt in ("-f", "--folder"):
+        if opt in ("-r", "--root"):
             folder = arg
-    list(folder)
     git_roots = []
-
-    for dirname, dirs, files in os.walk("/home/gulyasm"):
+    for dirname, dirs, files in os.walk(folder):
         if ".git" in dirs:
             git_roots.append(dirname)
             dirs.remove(".git")
@@ -28,7 +26,7 @@ def main(argv):
         g = git.Repo(d)
         changed_str = "Changed" if g.is_dirty() else "Not changed"
         if g.is_dirty():
-            print g.working_dir.replace("/home/gulyasm/", "", 1).ljust(70), g.active_branch.name.ljust(
+            print g.working_dir.replace(folder, "", 1).ljust(70), g.active_branch.name.ljust(
                 12), changed_str.ljust(20)
 
 
